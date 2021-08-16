35efa9a2cb459ab37cffa106d5bb8cd2682a3b6f
ABCXYZ <rnrmfjc@gmail.com>
2021-08-16 16:29:58 +0900

    asdf

--- front/vue_front/src/main.js
+++ front/vue_front/src/main.js
@@ -3,8 +3,19 @@ import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
 
 const app = createApp(App)
+
+let url = "http://localhost:8000/user/"; // 장고 서버 주소
+
+axios.get(url)
+.then(function(response){
+  console.log(response);
+})
+.catch(function(response){
+  console.log(response);
+})
 app.use(router)
 app.use(store)
 app.use(vuetify)
